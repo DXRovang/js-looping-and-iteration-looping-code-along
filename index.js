4bed92e7b966f5aee347a9c61a09d3752ab1a85c
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-12 18:33:43 -0400

    commit tree e3c92635bd8346b686bdbbf1cd4a866598c663fa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const arr = []
+function writeCards(array, string) {
+  for (let i = 0; i < array.length; i++) {
+    arr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+  }
+  return arr;
+}
+
+function countDown(number) {
+  for (let i = number; i >=0; i--){
+    console.log(i);
+  }
+}
